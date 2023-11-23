43e775778046469e0018290a519a692cf44e8363
zy <18270219411@163.com>
2023-11-23 14:56:50 +0800

    fix bug

--- src/Keys.jsx
+++ src/Keys.jsx
@@ -1,84 +1,9 @@
 import NewFrom from './components/NewFrom'
 import { Component } from 'react'
-import { Toast } from '@douyinfe/semi-ui';
-import { bitable } from '@lark-base-open/js-sdk'
 
-
-
-const base = bitable.base
-export default class         App extends Component {
+export default class App extends Component {
 
   state = {
-    keys: {},
-    history: [],
-    loading: false,
-    data: []
-  }
-  test = () => {
-    const { t } = this.props
-    console.log(t);
-  }
-  //用于添加一条数据
-  addItem = async (itemObj) => {
-    const { t } = this.props
-    const addSuccess = t('addSuccess')
-    const errorField = t('errorField')
-
-    try {
-      this.setState({ loading: true })
-      this.setState({ keys: itemObj })
-      const table = await base.getActiveTable()
-      const view = await table.getActiveView()
-      const fieldMetaList = await table.getFieldMetaList();
-
-      let mac = false
-      let win = false
-      // 判断是否有mac快捷键和win快捷键字段，如果没有则创建字段
-      fieldMetaList.forEach(item => {
-        if (item.name === 'mac快捷键') {
-          mac = true
-        }
-        if (item.name === 'win快捷键') {
-          win = true
-        }
-      })
-      if (!mac) {
-        await table.addField({ type: 1, name: 'mac快捷键' }) //新建文字字段
-      }
-      if (!win) {
-        await table.addField({ type: 1, name: 'win快捷键' }) //新建文字字段
-      }
-      // 添加记录
-      const fieldTitleList = await view.getVisibleFieldIdList()
-      let fieldTitle = fieldTitleList[0]
-      const fieldMac = await table.getField('mac快捷键')
-      const fieldWin = await table.getField('win快捷键')
-      const fieldTitleObj = await table.getFieldMetaById(fieldTitle)
-      if (fieldTitleObj.type !== 1) {
-        Toast.error({
-          content: errorField,
-          duration: 0,
-        })
-        return
-      }
-      // const res = 
-      await table.addRecord({
-        fields: {
-          [fieldTitleObj.id]: itemObj.title,
-          [fieldMac.id]: itemObj.mac.join(' + '),
-          [fieldWin.id]: itemObj.win.join(' + ')
-        }
-      })
-      await Toast.success({
-        content: addSuccess,
-        duration: 2,
-        showClose: false,
-      })
-    } finally {
-      this.setState({ loading: false })
-    }
-    // 撤销功能
-    // this.setState({ history: [res, ...this.state.history] })
   }
 
   render() {
@@ -88,7 +13,7 @@ export default class         App extends Component {
         <p className='tips'>
           {t('tips')}
         </p>
-        <NewFrom addItem={this.addItem} loading={this.state.loading} t={t} />
+        <NewFrom t={t} />
       </div>
     )
   }
--- src/components/NewFrom/index.jsx
+++ src/components/NewFrom/index.jsx
@@ -1,5 +1,5 @@
-import React, { Component, createRef } from "react"
-import { TextArea, Button, Toast, Typography, } from '@douyinfe/semi-ui';
+import React, { Component } from "react"
+import { TextArea, Button, Toast, Typography,  } from '@douyinfe/semi-ui';
 import KeyBoard from "../KeyBoard"
 import { bitable } from '@lark-base-open/js-sdk'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
@@ -9,53 +9,97 @@ const base = bitable.base
 let keyBoard = 1
 
 export default class NewFrom extends Component {
-    inputRef = createRef()
-    // 状态
     state = {
-        title: '', // 输入框中的标题文本
+        title: '',
         win: [],
         mac: [],
-        // checkLoading: false,
-        copied: false,
-        toast: null
+        loading: false,
+        toast: null,
     }
     // 添加数据
-    add = (event) => {
+    add = async () => {
+        this.setState({ loading: true })
+        // 获取翻译文本
         const { t } = this.props
         const toastAllEmpty = t('toastAllEmpty')
         const titleEmpty = t('titleEmpty')
         const keyEmpty = t('keyEmpty')
+        const addSuccess = t('addSuccess')
+        const errorField = t('errorField')
+        // 获取表格对象
+        const table = await base.getActiveTable()
+        const view = await table.getActiveView()
+        const fieldMetaList = await view.getFieldMetaList()
+        //判断是否有标题字段
+        const fieldTitle = fieldMetaList[0]
+        if (fieldTitle.type !== 1) {
+            Toast.error({
+                content: errorField,
+                duration: 5,
+            })
+            return this.setState({ loading: false })
+        }
+        // 判断快捷键是否为空
         const { inputTitleRef } = this
-        // 阻止默认事件
-        event.preventDefault();
-        // 判断
         const { mac, win } = this.state;
         if (inputTitleRef.value === '' && (mac.length === 0 && win.length === 0)) {
             Toast.warning({
                 content: toastAllEmpty,
                 duration: 2,
             })
-            return
+            return this.setState({ loading: false })
         } else if (inputTitleRef.value === '') {
             Toast.warning({
                 content: titleEmpty,
                 duration: 2,
             })
-            return
+            return this.setState({ loading: false })
         } else if (mac.length === 0 && win.length === 0) {
             Toast.warning({
                 content: keyEmpty,
                 duration: 2,
             })
-            return
+            return this.setState({ loading: false })
         }
-        this.props.addItem(this.state)
+        let macTitle = false
+        let winTitle = false
+        // 判断是否有mac快捷键和win快捷键字段，如果没有则创建字段
+        fieldMetaList.forEach(item => {
+            if (item.name === 'mac快捷键') {
+                macTitle = true
+            }
+            if (item.name === 'win快捷键') {
+                winTitle = true
+            }
+        })
+        if (!macTitle) {
+            await table.addField({ type: 1, name: 'mac快捷键' }) //新建文字字段
+        }
+        if (!winTitle) {
+            await table.addField({ type: 1, name: 'win快捷键' }) //新建文字字段
+        }
+        // 添加记录
+        const fieldMac = await table.getField('mac快捷键')
+        const fieldWin = await table.getField('win快捷键')
+        await table.addRecord({
+            fields: {
+                [fieldTitle.id]: this.state.title,
+                [fieldMac.id]: this.state.mac.join(' + '),
+                [fieldWin.id]: this.state.win.join(' + ')
+            }
+        })
         // 清空输入框
         keyBoard = keyBoard + 1
         this.setState({
             win: [],
             mac: [],
         })
+        await Toast.success({
+            content: addSuccess,
+            duration: 2,
+            showClose: false,
+        })
+        this.setState({ loading: false })
     }
     // 生成mac和win不同格式的快捷键
     keys = (e) => {
@@ -103,63 +147,60 @@ export default class NewFrom extends Component {
         }
     }
     // 获取当前表的记录
-    check = async (e) => {
-        try {
-            this.setState({ checkLoading: true })
-            let text = ''
-            const table = await base.getActiveTable()
-            const allRecords = await table.getRecords({
-                pageSize: 5000
+    check = async () => {
+        let text = ''
+        const table = await base.getActiveTable()
+        const allRecords = await table.getRecords({
+            pageSize: 5000
+        })
+        const records = allRecords.records
+        const fieldWin = await table.getFieldByName('win快捷键')
+        console.log(records);
+        for (let i = 0; i < records.length; i++) {
+            let winCell = records[i].fields[fieldWin.id]
+            let winCellValue = winCell ? winCell[0].text : ''
+            if (winCellValue === this.state.win.join(' + ')) {
+                text = winCellValue
+                break
+            }
+        }
+        const { t } = this.props
+        const keyDisabled1 = t('keyDisabled1')
+        if (text === '') {
+            return true
+        } else {
+            const { Text } = Typography
+            let toast = Toast.warning({
+                content: (
+                    <span>
+                        <div>{keyDisabled1}</div>
+                        <CopyToClipboard text={text}>
+                            <Text link>复制快捷键</Text>
+                        </CopyToClipboard>
+                        <Text link onClick={this.goOn} style={{ marginLeft: '10px' }}>继续添加</Text>
+                    </span>
+                ),
+                duration: 0,
             })
-            const records = allRecords.records
-            const fieldWin = await table.getFieldByName('win快捷键')
-            for (let i = 0; i < records.length; i++) {
-                let winCellValue = records[i].fields[fieldWin.id][0].text
-                if (winCellValue === this.state.win.join(' + ')) {
-                    text = winCellValue
-                    break
-                }
-            }
-            const { t } = this.props
-            const keyDisabled1 = t('keyDisabled1')
-            if (text === '') {
-                return true
-            } else {
-                const { Text } = Typography
-                let toast = Toast.warning({
-                    content: (
-                        <span>
-                            <div>{keyDisabled1}</div>
-                            <CopyToClipboard text={text}
-                                onCopy={() => this.setState({ copied: true })}>
-                                <Text link>复制快捷键</Text>
-                            </CopyToClipboard>
-                            <Text link onClick={this.goOn} style={{ marginLeft: '10px' }}>继续添加</Text>
-                        </span>
-                    ),
-                    duration: 0,
-                })
-                this.setState({ toast: toast })
-            }
-
-        } finally {
-            this.setState({ checkLoading: false })
+            this.setState({ toast: toast })
         }
     }
-    goOn = async (e) => {
+    goOn = async () => {
+        this.setState({ loading: true })
         Toast.close(this.state.toast)
-        this.setState({ toast: null }) 
-        this.add(e)
+        this.setState({ toast: null })
+        this.add()
     }
-    addAndCheck = async (e) => {
-        const result = await this.check(e)
-        if (!result) return
-        await this.add(e)
+    addAndCheck = async () => {
+        this.setState({ loading: true })
+        const result = await this.check()
+        if (!result) return this.setState({ loading: false })
+        await this.add()
     }
     render() {
         const { t } = this.props
         return (
-            <form name="mydata" className="gridAdd">
+            <div name="mydata" className="gridAdd">
                 <TextArea key={keyBoard + 1} autosize={{ minRows: 1, maxRows: 4 }}
                     className="addInput"
                     placeholder={t('placeholderTitle')}
@@ -168,10 +209,8 @@ export default class NewFrom extends Component {
                 />
                 <KeyBoard className="addInput" keys={this.keys} key={keyBoard} t={t} />
                 <br />
-                <Button size="large" theme="solid" className="addInput" onClick={this.addAndCheck} loading={this.props.loading}>{t('add')}</Button>
-                {/* <Button loading={this.state.checkLoading} size="large" className="addInput" onClick={this.check}>{t('check')}</Button> */}
-
-            </form>
+                <Button size="large" theme="solid" className="addInput" onClick={this.addAndCheck} loading={this.state.loading}>{t('add')}</Button>
+            </div>
         )
     }
 }
